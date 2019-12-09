afa49915a1aa10176d50e484a716eb38b786acf6
Bingji Guo <bingjig2@illinois.edu>
2019-12-09 11:42:44 -0600

    improvement: trim quotation

--- app/src/main/java/com/example/sudokuforprogrammer/NewGameActivity.java
+++ app/src/main/java/com/example/sudokuforprogrammer/NewGameActivity.java
@@ -9,6 +9,7 @@ import android.graphics.fonts.FontFamily;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
